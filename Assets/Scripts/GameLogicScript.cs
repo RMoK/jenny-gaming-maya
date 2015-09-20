924a3a5af2115b612abfb9121c37fba3a9b0f1ea
Jenny Jiao Hsia <jj1317@nyu.edu>
2015-09-20 13:43:42 -0400

    2nd commit?!

---
+++ Assets/Scripts/GameLogicScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameLogicScript : MonoBehaviour {
+
+	public Transform treasure;
+	public Transform trees; 
+	public Transform rock; 
+	public Transform startingPoint; 
+	public Transform player;
+	public Text myText;
+
+
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// starting point
+		if ((startingPoint.position - player.position).magnitude < 10f) {
+			myText.text = "Use WASD to navigate around this ocean. Press SPACE when you've found the treasure!";
+			Debug.Log("start");
+		} else if ((trees.position - player.position).magnitude < 10f) {
+			myText.text = "Go Southwest until you see a big upside-down, U-shaped rock.";
+		} 
+
+		else if ((rock.position - player.position).magnitude < 10f) {
+			myText.text = "Keep going East until you see an island with 1 palm tree. The treasure is nearby.";
+		} 
+
+		// winning 
+		else if ((treasure.position - player.position).magnitude < 10f) {
+			//Debug.Log ("YOU WIN");
+			if (Input.GetKey(KeyCode.Space)) {
+				myText.text = "CONGRATULATIONS, YOU WIN!!!";
+			}
+		}
+
+		else {
+			myText.text = "";
+
+		}
+
+	
+	}
+}
---
+++ Assets/Scripts/PlayerInputScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInputScript : MonoBehaviour {
+
+	public float moveSpeed = 5f; 
+	public float turnSpeed = 10f;
+
+	Vector3 cameraPos = new Vector3(0f, 2.5f, -3.66f);
+	Quaternion targetRotation = Quaternion.Euler(0f, 0f, 0f);
+	//public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Quaternion currentRotation = transform.rotation;
+		//Quaternion targetRotation = Quaternion.Euler (blah, blah, blah);
+		//transform.rotation = Quaternion.Slerp (currentRotation, targetRotation, Time.deltaTime*something);
+
+		Quaternion currentRotation = transform.rotation;
+
+		if ( Input.GetKey ( KeyCode.W ) ) {
+			// move the current object, framerate independent
+			transform.position += new Vector3(0f, 0f, 1f) * Time.deltaTime * moveSpeed; 
+
+			// rotate the current object to face the way it's moving
+			//transform.eulerAngles = new Vector3(0f, 0f, 0f); 
+			targetRotation = Quaternion.Euler(0f, 0f, 0f);
+
+
+	
+		} else if ( Input.GetKey ( KeyCode.S ) ) {
+			transform.position += new Vector3(0f, 0f, -1f) * Time.deltaTime * moveSpeed;
+			//transform.eulerAngles = new Vector3(0f, -180f, 0f); 
+			//transform.rotation = Quaternion.Euler(0f, -180f, 0f);
+			targetRotation = Quaternion.Euler(0f, -180f, 0f);
+
+
+		} else if ( Input.GetKey (KeyCode.A) ) { // move to your left
+			transform.position += new Vector3 (-1f, 0f, 0f) * Time.deltaTime * moveSpeed;
+			//transform.eulerAngles = new Vector3(0f, -90f, 0f); 
+			targetRotation = Quaternion.Euler(0f, -90f, 0f);
+
+		} else if ( Input.GetKey ( KeyCode.D ) ) {
+			transform.position += new Vector3 (1f, 0f, 0f) * Time.deltaTime * moveSpeed;
+			//transform.eulerAngles = new Vector3(0f, 90f, 0f); 
+			targetRotation = Quaternion.Euler(0f, 90f, 0f);
+
+		} 
+
+		transform.rotation = Quaternion.Slerp (currentRotation, targetRotation, Time.deltaTime*turnSpeed);
+
+		// move main camera to player's mosition, plus an offset above it
+		Camera.main.transform.position = transform.position + cameraPos;
+
+
+	
+	}
+}
